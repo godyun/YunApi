2acf156a4312d302d9623d6e0f408f9cd3cc3e0f
god <308542040@qq.com>
2019-05-19 21:45:04 +0800

    Bug修复

--- application/model/weacher.php
+++ application/model/weacher.php
@@ -1,30 +1,12 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: cnmobi
- * Date: 2017/5/31
- * Time: 16:44
+ * @since   2017-11-02
+ * @author  zhaoxiang <zhaoxiang051405@gmail.com>
  */
-namespace app\api\controller;
-class Weather extends Base 
-{
-    /**
-     * 获取城市天气预报
-     * @return bool|string
-     */
-    public function get_weather()
-    {
-		$city = $this->request->param ( "city" );
-        $res = file_get_contents("http://api.map.baidu.com/telematics/v3/weather?location={[$city}&output=json&ak=32da004455c52b48d84a3a484c0dbc99");
-		 
-		 
-		 return json_decode($res, true);
-		/* if ($res) {
-            return $this->buildSuccess ( $res->toArray () );
-        } else {
-            return $this->buildFailed ( ReturnCode::NOT_EXISTS, "当前用户不存在" );
-        } */
-        
-    }
+
+namespace app\model;
+
+
+class Wercher extends Base {
 
 }
