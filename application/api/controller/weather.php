c12ce882ebb1cd4e04198641718c9a81fffd4c9e
god <308542040@qq.com>
2019-05-19 15:14:26 +0800

    天气查询接口

--- application/api/controller/weather.php
+++ application/api/controller/weather.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Created by 贾守彬.
+ * Created by god.
  * User: cnmobi
  * Date: 2019/5/18
  * Time: 16:44
@@ -8,16 +8,16 @@
 namespace app\api\controller;
 use app\util\ReturnCode;
 
-class User weather Base {
+class Weather extends  Base {
     public function getWeatherinfo(){
         $city = $this->request->param ( "city" );
-			
         $weatherinfo = file_get_contents("http://api.map.baidu.com/telematics/v3/weather?location=$city&output=json&ak=32da004455c52b48d84a3a484c0dbc99");
-		
+        $weatherinfo =str_replace( "error\":0,\"","",$weatherinfo);
+        $weatherinfo =str_replace( "\"status\":\"success\",","",$weatherinfo);
+        $weatherinfo =json_decode($weatherinfo , true);
         if ($weatherinfo) {
-            return $this->buildSuccess ( $weatherinfo ->toArray ());
-	$this->debug($weatherinfo);
-        } else {
+            return $this->buildSuccess ( $weatherinfo);
+	        } else {
             return $this->buildFailed ( ReturnCode::NOT_EXISTS, "查询出错" );
         }
     }
